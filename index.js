49dd2476b1b7530443ccca973435c021661bb074
Umut Yurdugül <102877643+vangougha@users.noreply.github.com>
2024-06-30 14:10:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [2923300,2784840];  // Enter here AppIDs of the needed games
+var games = [2923300,2784840,730, 1506830, 223470,394360,401680,227300,413150,232770,48700];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
-//730, 1506830, 223470,394360,401680,227300,413150,232770,48700]
+//]
 
 user = new steamUser();
 user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
